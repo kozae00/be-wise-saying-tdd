68009f9be4f3ab4e2ded511b3620536680e9c43f
kozae00 <rhwogus777@gmail.com>
2025-01-02 15:45:04 +0900

    commit4 : Stream

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,36 @@
 package org.example;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 입력을 개발자가 아닌, 봇에게 맡길 수 있다.
+        test2();
+    }
+
+    public static void test2() {
+        // System.out -> 표준 출력 -> 모니터(콘솔)
+
+        // 2. 출력문의 값을 문자열로 받아서 봇에게 줄 수 있다.
+        PrintStream origin = System.out;
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream(); // 비어있는 스트림
+        System.setOut(new PrintStream(out));
+
+        System.out.println("hello");
+
+        System.setOut(origin);
+
+        String str = out.toString();
+        System.out.println(str);
+
+    }
+
+    public static void test1() {
+        // System.in --> 표준 입력
+
+        // 1. 입력을 봇에게 맡기 수 있다.
         Scanner scan = new Scanner("등록\n현재를 사랑하라.\n작자미상\n");
 
         String val1 = scan.nextLine();
@@ -16,7 +42,7 @@ public class Main {
         String val3 = scan.nextLine();
         System.out.println(val3);
 
-        // 테스트봇 만들기
+        //테스트봇 만들기
 
         TestApp app = new TestApp();
         app.run();
