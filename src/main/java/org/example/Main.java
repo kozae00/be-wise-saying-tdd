d746cfd38fbc84a24f138584145a0d5bcd0aa57d
kozae00 <rhwogus777@gmail.com>
2025-01-02 14:54:29 +0900

    commit1 : TestBot

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력을 개발자가 아닌, 봇에게 맡길 수 있다.
+        Scanner scan = new Scanner("등록\n현재를 사랑하라.\n작자미상\n");
+
+        String val1 = scan.nextLine();
+        System.out.println(val1);
+
+        String val2 = scan.nextLine();
+        System.out.println(val2);
+
+        String val3 = scan.nextLine();
+        System.out.println(val3);
+
+        // 테스트봇 만들기
+    }
+}
