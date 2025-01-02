91d030200ed2aad18cc169d139938b856d6c4f06
kozae00 <rhwogus777@gmail.com>
2025-01-02 15:13:43 +0900

    commit2 : @Test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FirstTest {
+
+    @Test
+    void t1() {
+
+        assertThat(2).isEqualTo(1); // 예상값과 실제값 비교
+    }
+}
