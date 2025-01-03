e908c343199601a3f7dd718bc3e88eaa7daf348d
kozae00 <rhwogus777@gmail.com>
2025-01-03 12:49:16 +0900

    commit10 : @test5

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -25,7 +25,7 @@ public class FirstTest {
     @Test
     void t3() {
         // 테스트봇 선입력
-        String out = TestBot.run("종료");
+        String out = TestBot.run("");
 
         assertThat(out.toString()).contains("명언앱을 종료합니다.");
 
@@ -36,7 +36,7 @@ public class FirstTest {
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
     void t4() {
         // 테스트봇 선입력
-        String out = TestBot.run("종료");
+        String out = TestBot.run("");
 
         assertThat(out.toString())
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
@@ -48,7 +48,11 @@ public class FirstTest {
     @DisplayName("등록 - 명언 1개 입력")
     void t5() {
         // 테스트봇 선입력
-        String out = TestBot.run("등록\n현재를 사랑하라.\n작자미상\n종료");
+        String out = TestBot.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
 
         assertThat(out.toString())
                 .containsSubsequence("명언 : ", "작가 : ");
--- src/test/java/TestBot.java
+++ src/test/java/TestBot.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class TestBot {
 
     public static String run(String input) {
-        Scanner sc = new Scanner(input + "\n");
+        Scanner sc = new Scanner(input + "종료\n");
 
         ByteArrayOutputStream out = new ByteArrayOutputStream(); // 비어있는 스트림
         System.setOut(new PrintStream(out));
