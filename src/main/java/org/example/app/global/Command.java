3eeee4ac576a0d8d97a6a4c9af1a5debdf3ba775
kozae00 <rhwogus777@gmail.com>
2025-01-06 10:44:02 +0900

    commit26 : refactoring

--- src/main/java/org/example/app/global/Command.java
+++ src/main/java/org/example/app/global/Command.java
@@ -3,13 +3,31 @@ package org.example.app.global;
 public class Command {
 
     String actionName;
+    String paramKey;
+    String paramValue;
+
 
     public Command(String cmd) {
-        String[] cmdBits = cmd.split("\\?");
+        // 이름 = 차태진(key-value)
+        String[] cmdBits = cmd.split("\\?"); // ["삭제", "id=1"]
         actionName = cmdBits[0];
+
+        if(cmdBits.length < 2) {
+            paramValue = "";
+            return;
+        }
+
+        String param = cmdBits[1];
+        String[] paramBits = param.split("=");
+        paramKey = paramBits[0];
+        paramValue = paramBits[1];
     }
 
     public String getActionName() {
         return actionName;
+    }
+
+    public int getParam() {
+        return Integer.parseInt(paramValue);
     }
 }
