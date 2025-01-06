a583dd58c2c3279b1607116339a958f3759fa2b1
kozae00 <rhwogus777@gmail.com>
2025-01-06 14:29:10 +0900

    commit31 : read

--- src/main/java/org/example/app/standard/Util.java
+++ src/main/java/org/example/app/standard/Util.java
@@ -1,10 +1,39 @@
 package org.example.app.standard;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Util {
 
     public static class File{
         public static void test(){
             System.out.println("파일 유틸 테스트");
+        }
+
+        public static void createFile(String pathValue) {
+            Path filePath = Paths.get(pathValue);
+
+            try {
+                Files.createFile(filePath);
+            } catch (Exception e) {
+                System.out.println("파일 생성 중 실패");
+                e.printStackTrace();
+            }
+        }
+
+        public static String readAsString(String file) {
+
+            Path filePath = Paths.get(file);
+
+            try {
+                return Files.readString(filePath);
+            } catch (IOException e) {
+                System.out.println("파일 읽기 실패");
+                e.printStackTrace();
+            }
+            return "";
         }
     }
 }
--- src/test/java/app/standard/FileTest.java
+++ src/test/java/app/standard/FileTest.java
@@ -4,7 +4,10 @@ import org.example.app.standard.Util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FileTest {
 
@@ -13,4 +16,41 @@ public class FileTest {
     void t1() {
         Util.File.test();
     }
+
+    @Test
+    @DisplayName("파일 생성. 내용이 없는 빈 파일 생성")
+    void t2() {
+        String file = "test.txt";
+        
+        Util.File.createFile(file);
+
+
+        assertThat(Files.exists(Paths.get(file)))
+                .isTrue();
+    }
+
+    @Test
+    @DisplayName("파일 내용 읽어 오기")
+    void t3() {
+        String testContent = "Hello, world";
+
+        String file = "test.txt";
+        String content = Util.File.readAsString(file);
+
+        assertThat(Files.exists(Paths.get(file)))
+                .isTrue();
+    }
+
+//    @Test
+//    @DisplayName("파일 생성. 내용이 없는 빈 파일 생성")
+//    void t3() {
+//        String file = "test.txt";
+//
+//        Util.File.wirte(file, "Hello, World!");
+//
+//        String content = Files.readString(Paths.get(file));
+//
+//        assertThat(Files.exists(Paths.get(file)))
+//                .isTrue();
+//    }
 }
