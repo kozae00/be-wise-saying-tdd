0780a4e0e723ed1385d2302e80cf98b7b7d262b6
kozae00 <rhwogus777@gmail.com>
2025-01-15 12:03:45 +0900

    t1 r

--- src/main/java/org/example/app/domain/wiseSaying/repository/WiseSayingDbRepository.java
+++ src/main/java/org/example/app/domain/wiseSaying/repository/WiseSayingDbRepository.java
@@ -2,6 +2,9 @@ package org.example.app.domain.wiseSaying.repository;
 
 import org.example.app.domain.wiseSaying.WiseSaying;
 import org.example.app.standard.simpleDb.SimpleDb;
+import org.example.app.standard.simpleDb.Sql;
+
+import java.util.Optional;
 
 public class WiseSayingDbRepository {
 
@@ -24,5 +27,26 @@ public class WiseSayingDbRepository {
     }
 
     public void truncateWiseSayingTable() {
+        simpleDb.run("TRUNCATE article");
+    }
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        Sql sql = simpleDb.genSql();
+        sql.append("INSERT INTO wise_saying ")
+                .append("SET content = ?", wiseSaying.getContent())
+                .append("author = ?", wiseSaying.getAuthor());
+        sql.insert();
+
+        return wiseSaying;
+    }
+
+    public Optional<WiseSaying> findById(int id) {
+
+        Sql sql = simpleDb.genSql();
+        sql.append("SELECT *")
+                .append("FROM wise_saying")
+                .append("WHERE id = ?", id);
+        WiseSaying wiseSaying = sql.selectRow(WiseSaying.class);
+        return Optional.of(wiseSaying);
     }
 }
