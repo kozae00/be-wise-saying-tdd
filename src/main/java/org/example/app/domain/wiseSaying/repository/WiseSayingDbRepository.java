f483d77b19df998089e9b84109e8b1f553054e0a
kozae00 <rhwogus777@gmail.com>
2025-01-15 12:44:11 +0900

    dbrepo t5 g

--- src/main/java/org/example/app/domain/wiseSaying/repository/WiseSayingDbRepository.java
+++ src/main/java/org/example/app/domain/wiseSaying/repository/WiseSayingDbRepository.java
@@ -90,4 +90,11 @@ public class WiseSayingDbRepository {
     public static String getBuildPath() {
         return DB_PATH;
     }
+
+    public long count() {
+        return simpleDb.genSql()
+                .append("SELECT COUNT(*)")
+                .append("FROM wise_saying")
+                .selectLong();
+    }
 }
